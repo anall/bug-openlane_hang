a4f0ce056826184cf5ad5ee67e66ae578bb8efd1
Andrea Nall <anall@andreanall.com>
2021-10-05 12:54:05 -0500

    modify example for yosys-abc segfault

--- src/opensta_hang.v
+++ src/opensta_hang.v
@@ -2,26 +2,15 @@
 `timescale 1ns/1ns
 module opensta_hang(
   input wire clk,
-  input wire reset,
 
   inout wire data,
 
-  input wire start,
   input wire we,
   input wire oe,
 );
 
+reg busy;
 wire data_c = we ? ( busy ? 1 : data ) : 'z;
 assign data = (oe&~busy) ? data_c : 'z;
-
-reg busy;
-
-always @(posedge clk) begin
-  if ( reset ) begin
-    busy <= 0;
-  end else begin
-    busy <= 1;
-  end
-end
 
 endmodule
