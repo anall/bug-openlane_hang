bddf0ca89a8424339d08828b960bd61ca915736c
Andrea Nall <anall@andreanall.com>
2021-10-05 09:54:42 -0500

    add config.tcl

---
+++ config.tcl
@@ -0,0 +1,18 @@
+# User config
+set ::env(DESIGN_NAME) fib
+
+# Change if needed
+set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
+
+# Fill this
+set ::env(CLOCK_PERIOD) "10.0"
+set ::env(CLOCK_PORT) "clk"
+
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 200 200"
+
+set filename $::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+if { [file exists $filename] == 1} {
+	source $filename
+}
+
